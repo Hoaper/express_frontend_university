b139f858fa13bea0faa9559d92d8a9f9e45f9e79
Hoaper <beeboplay@gmail.com>
2024-02-24 17:36:18 +0600

    localstorage fix 8

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -3,18 +3,20 @@ import React from 'react';
 import {useRouter} from "next/navigation";
 
 function Layout({ children }: { children: React.ReactNode }) {
-    const token = typeof window !== 'undefined' ? localStorage.getItem('token') as string : "";
     const router = useRouter();
-    if (!token) {
-        router.push("/login");
-        return;
-    } else {
-        return (
-            <div>
-                {children}
-            </div>
-        );
-    }
+
+    React.useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            router.push('/login');
+        }
+    }, []);
+
+    return (
+        <div>
+            {children}
+        </div>
+    );
 }
 
 export default Layout;
